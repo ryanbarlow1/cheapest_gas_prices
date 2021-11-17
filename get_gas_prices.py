1cf4e0387738fd4970f8472055d5879da5054f12
Ryan Barlow <barlow.ryan@rocketmail.com>
2021-11-16 20:09:34 -0700

    Updated to use new graphql endpoint

--- get_gas_prices.py
+++ get_gas_prices.py
@@ -1,43 +1,32 @@
 import requests
 import pandas
 import datetime_translator
- 
-brandIds = { 202, 88, 31, 123, 101, 122, 36, 48, 135, 170 }
- 
-stationsUrl = 'https://www.gasbuddy.com/assets-v2/api/stations?area=salt-lake&countyId=1002&regionCode=UT&countryCode=US&fuel=1&maxAge=0'
-#stationsUrl = 'https://www.gasbuddy.com/assets-v2/api/stations?area=davis&countyId=1165&regionCode=UT&countryCode=US&fuel=1&maxAge=0'
-#stationsUrl = 'https://www.gasbuddy.com/assets-v2/api/stations?area=weber&countyId=881&regionCode=UT&countryCode=US&fuel=1&maxAge=0'
- 
-headers = { 'User-Agent': '' }
-stationsResponse = requests.get(stationsUrl, headers=headers)
-stations = stationsResponse.json()
- 
-fuelsUrl = 'https://www.gasbuddy.com/assets-v2/api/fuels?fuel=1'
- 
+
+brandIds = { 202, 88, 31, 123, 101, 122, 36, 48, 135 }
 data = {}
- 
-for station in stations['stations']:
-    if station['brand_id'] in brandIds:
-        stationId = station['id']
-        fuelsUrl += '&stationIds=' + str(stationId)
-        data[stationId] = {
-            'StationName': station['name'],
-            'BrandName': station['brand_name'],
-            'AddressLine1': station['address']['line_1'],
-            'City': station['address']['locality']
-        }
- 
-fuelsResponse = requests.get(fuelsUrl, headers=headers)
-fuels = fuelsResponse.json()
- 
-for fuel in fuels['fuels']:
-    if fuel['fuelType'] == 'regular_gas':
-        stationId = int(fuel['stationId'])
-        for price in fuel['prices']:
-            if ((price['isCash'] == False) or (len(price) == 1)) and (price['price'] != 0):
-                data[stationId]['RegularFuelPrice'] = price['price']
-                data[stationId]['ReportedBy'] = price['reportedBy']
-                data[stationId]['DaysSinceReported'] = datetime_translator.translate(price['postedTime'])
+
+for brandId in brandIds:
+    headers = { 'User-Agent': '', 'content-type':	'application/json' }
+    jsonData = '{"variables":{"area":"salt-lake","brandId":%d,"countryCode":"US","criteria":{"location_type":"county"},"fuel":1,"maxAge":0,"regionCode":"UT"},"query":"query LocationByArea($area: String, $brandId: Int, $countryCode: String, $criteria: Criteria, $fuel: Int, $maxAge: Int, $regionCode: String) { locationByArea( area: $area countryCode: $countryCode criteria: $criteria regionCode: $regionCode ) { displayName locationType stations(brandId: $brandId, fuel: $fuel, maxAge: $maxAge) { results { address { country line_1 line_2 locality postal_code region  } brands { brand_id branding_type image_url name  } latitude longitude fuels id name prices(fuel: $fuel) { cash { nickname posted_time price  } credit { nickname posted_time price  } discount fuel_product } } } } }"}' %(brandId)
+    #jsonData = '{"variables":{"area":"davis","brandId":%d,"countryCode":"US","criteria":{"location_type":"county"},"fuel":1,"maxAge":0,"regionCode":"UT"},"query":"query LocationByArea($area: String, $brandId: Int, $countryCode: String, $criteria: Criteria, $fuel: Int, $maxAge: Int, $regionCode: String) { locationByArea( area: $area countryCode: $countryCode criteria: $criteria regionCode: $regionCode ) { displayName locationType stations(brandId: $brandId, fuel: $fuel, maxAge: $maxAge) { results { address { country line_1 line_2 locality postal_code region  } brands { brand_id branding_type image_url name  } latitude longitude fuels id name prices(fuel: $fuel) { cash { nickname posted_time price  } credit { nickname posted_time price  } discount fuel_product } } } } }"}' %(brandId)
+    response = requests.post('https://www.gasbuddy.com/graphql', headers=headers, data=jsonData)
+    jsonResponse = response.json()
+    stations = jsonResponse['data']['locationByArea']['stations']
+
+    for station in stations['results']:
+        if int(station['brands'][0]['brand_id']) in brandIds and (station['prices'][0]['credit']['price'] != 0):
+            stationId = station['id']
+            data[stationId] = {
+                'StationName': station['name'],
+                'BrandName': station['brands'][0]['name'],
+                'AddressLine1': station['address']['line_1'],
+                'City': station['address']['locality'],
+                'RegularFuelPrice': '${:.2f}'.format(station['prices'][0]['credit']['price']),
+                'TimeSinceReported': datetime_translator.translate(station['prices'][0]['credit']['posted_time']),
+                'ReportedBy': station['prices'][0]['credit']['nickname']
+            }
 
 output = pandas.DataFrame(data)
 output.transpose().sort_values(by='RegularFuelPrice')
+
+print(output)
