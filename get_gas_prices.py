f8831939f992cb6e6d882bd3399a8b8a837f113a
Ryan Barlow <barlow.ryan@rocketmail.com>
2023-09-07 20:45:28 -0600

    Fixed GraphQL query with updated field names

--- get_gas_prices.py
+++ get_gas_prices.py
@@ -8,14 +8,14 @@ data = {}
 
 for brandId in brandIds:
     headers = { 'User-Agent': '', 'content-type': 'application/json' }
-    jsonData = '{"variables":{"area":"salt-lake","brandId":%d,"countryCode":"US","criteria":{"location_type":"county"},"fuel":1,"maxAge":0,"regionCode":"UT"},"query":"query LocationByArea($area: String, $brandId: Int, $countryCode: String, $criteria: Criteria, $fuel: Int, $maxAge: Int, $regionCode: String) { locationByArea( area: $area countryCode: $countryCode criteria: $criteria regionCode: $regionCode ) { displayName locationType stations(brandId: $brandId, fuel: $fuel, maxAge: $maxAge) { results { address { country line1 line2 locality postalCode region  } brands { brand_id branding_type image_url name  } latitude longitude fuels id name prices(fuel: $fuel) { cash { nickname posted_time price  } credit { nickname posted_time price  } discount fuel_product } } } } }"}' %(brandId)
-    #jsonData = '{"variables":{"area":"davis","brandId":%d,"countryCode":"US","criteria":{"location_type":"county"},"fuel":1,"maxAge":0,"regionCode":"UT"},"query":"query LocationByArea($area: String, $brandId: Int, $countryCode: String, $criteria: Criteria, $fuel: Int, $maxAge: Int, $regionCode: String) { locationByArea( area: $area countryCode: $countryCode criteria: $criteria regionCode: $regionCode ) { displayName locationType stations(brandId: $brandId, fuel: $fuel, maxAge: $maxAge) { results { address { country line1 line2 locality postalCode region  } brands { brand_id branding_type image_url name  } latitude longitude fuels id name prices(fuel: $fuel) { cash { nickname posted_time price  } credit { nickname posted_time price  } discount fuel_product } } } } }"}' %(brandId)
+    jsonData = '{"variables":{"area":"salt-lake","brandId":%d,"countryCode":"US","criteria":{"location_type":"county"},"fuel":1,"maxAge":0,"regionCode":"UT"},"query":"query LocationByArea($area: String, $brandId: Int, $countryCode: String, $criteria: Criteria, $fuel: Int, $maxAge: Int, $regionCode: String) { locationByArea( area: $area countryCode: $countryCode criteria: $criteria regionCode: $regionCode ) { displayName locationType stations(brandId: $brandId, fuel: $fuel, maxAge: $maxAge) { results { address { country line1 line2 locality postalCode region  } brands { brandId brandingType imageUrl name  } latitude longitude fuels id name prices(fuel: $fuel) { cash { nickname postedTime price  } credit { nickname postedTime price  } discount fuelProduct } } } } }"}' %(brandId)
+    #jsonData = '{"variables":{"area":"davis","brandId":%d,"countryCode":"US","criteria":{"location_type":"county"},"fuel":1,"maxAge":0,"regionCode":"UT"},"query":"query LocationByArea($area: String, $brandId: Int, $countryCode: String, $criteria: Criteria, $fuel: Int, $maxAge: Int, $regionCode: String) { locationByArea( area: $area countryCode: $countryCode criteria: $criteria regionCode: $regionCode ) { displayName locationType stations(brandId: $brandId, fuel: $fuel, maxAge: $maxAge) { results { address { country line1 line2 locality postalCode region  } brands { brandId brandingType imageUrl name  } latitude longitude fuels id name prices(fuel: $fuel) { cash { nickname postedTime price  } credit { nickname postedTime price  } discount fuelProduct } } } } }"}' %(brandId)
     response = requests.post('https://www.gasbuddy.com/graphql', headers=headers, data=jsonData)
     jsonResponse = response.json()
     stations = jsonResponse['data']['locationByArea']['stations']
 
     for station in stations['results']:
-        if int(station['brands'][0]['brand_id']) in brandIds and (station['prices'][0]['credit']['price'] != 0):
+        if int(station['brands'][0]['brandId']) in brandIds and (station['prices'][0]['credit']['price'] != 0):
             stationId = station['id']
             data[stationId] = {
                 'StationName': station['name'],
@@ -23,7 +23,7 @@ for brandId in brandIds:
                 'AddressLine1': station['address']['line1'],
                 'City': station['address']['locality'],
                 'RegularFuelPrice': '${:.2f}'.format(station['prices'][0]['credit']['price']),
-                'TimeSinceReported': datetime_translator.translate(station['prices'][0]['credit']['posted_time']),
+                'TimeSinceReported': datetime_translator.translate(station['prices'][0]['credit']['postedTime']),
                 'ReportedBy': station['prices'][0]['credit']['nickname']
             }
 
